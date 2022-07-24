8a50624dfbb81dbd57bc054a33a31557864bf46d
donghohnation <donghoh.han@gmail.com>
2022-07-23 19:46:22 -0700

    updated code and styling

--- src/components/BodySections/Hero.jsx
+++ src/components/BodySections/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+// import ava from "../../assets/images/Avatar-3_1-5.svg"
 
 
 
@@ -7,7 +7,12 @@ function Hero() {
 
 	return (
 		<div className="row rowow">
+			{/* <div className="text-center">
+				<img id="avatar" src={ava} alt='avatar'></img>
+			</div> */}
 			<div className="col d1 dotted-spaced">
+				
+				
 				<h1 id="title">HI, I'M DONGHOH.</h1>
 				<p id="titlecaption">California based Graphic and UI/UX Designer and Web Developer.</p>
 			</div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ava from "../assets/images/Avatar-3_1-5.svg"
 
 
 
@@ -51,7 +52,7 @@ function Nav() {
 		color: ""
 	};
 	
-	if (currentTime < 12) {
+	if (currentTime < 12 && currentTime > 4) {
 		greeting = "GOOD MORNING";
 	} else if (currentTime < 18) {
 		greeting = "GOOD AFTERNOON";
@@ -65,28 +66,39 @@ function Nav() {
 
 
 	return (
-		<div className="row">
+		<div>
+			<div className="row">
+			
 
 
-			<div className="col col-sm">
-				<h1 id="greettime" style={customStyle}>
-					{greeting} <span id="time"></span>
-				</h1>
+				<div className="col col-sm">
+					<h1 id="greettime" style={customStyle}>
+						{greeting} <span id="time"></span>
+					</h1>
+				</div>
+
+
+				<div className="col col-sm-2 nav">
+					<ul>
+						<li className="dash">_</li>
+						<li><a href="#About Me">About Me</a></li>
+						<li><a href="#Work">Work</a></li>
+						<li><a href="#Contact">Contact</a></li>
+						<li><a href="#Resume">Resume</a></li>
+						<li><a href="#Top">Top</a></li>
+						<br></br>
+						<br></br>
+						<li><a class="socials" href="https://www.linkedin.com/in/donghoh-han-7b3a4656/">LINKEDIN</a></li>
+						<li><a class="socials" href="https://github.com/donghohnation">GITHUB</a></li>
+						<li><a class="socials" href="https://www.behance.net/donghohnation/">BEHANCE</a></li>
+					</ul>
+				</div>
+
+
 			</div>
-
-
-			<div className="col col-sm-2 nav">
-				<ul>
-					<li className="dash">_</li>
-					<li><a href="#About Me">About Me</a></li>
-					<li><a href="#Work">Work</a></li>
-					<li><a href="#Contact">Contact</a></li>
-					<li><a href="#Resume">Resume</a></li>
-					<li><a href="#Top">Top</a></li>
-				</ul>
+			<div>
+					<img id="avatar" src={ava} alt='avatar'></img>
 			</div>
-
-
 		</div>
 	)
 
