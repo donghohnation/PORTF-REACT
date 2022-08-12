df164965895a99c3dcae7fdd1f0327f84dca39e1
donghohnation <donghoh.han@gmail.com>
2022-08-11 20:29:31 -0700

    dark mode toggle added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,129 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
 import Nav from "./Nav";
 import Body from "./Body";
+import ava from "../assets/images/Avatar-3_1-5.svg"
+
 // import Head from "./Head";
+
+export const ThemeContext = createContext(null);
+
+
 
 
 
 function App() {
 
-	const [isActive, setIsActive] = useState(false);
 
-	const handleClick = () => {
-		// 👇️ toggle
-		setIsActive(current => !current);
+
+
+	const [theme, setTheme] = useState("light");
+
+	const toggleTheme = () => {
+		setTheme((curr) => (curr === "light" ? "dark" : "light"));
+	};
+
+
+
+	
+
+	const date = new Date();
+	const currentTime = date.getHours();
+
+
+	function theTime() {
+		let date = new Date();
+		let rightnow = date.toLocaleTimeString();
+		document.getElementById("time").textContent = `${rightnow}`
+	}
+	setInterval(theTime);
+
+	let greeting;
+
+	const customStyle = {
+		color: ""
+	};
+
+	if (currentTime < 12 && currentTime > 4) {
+		greeting = "GOOD MORNING";
+	} else if (currentTime < 18) {
+		greeting = "GOOD AFTERNOON";
+	} else if (currentTime < 22) {
+		greeting = "GOOD EVENING";
+	} else {
+		greeting = "GOOD NIGHT";
 	}
 
 
-    return (
-		<div className="bodybackground" style={{ backgroundColor: isActive ? 'orange' : '', color: isActive ? 'white' : '', }}>
 
-			{/* <div>
-				<button onClick={handleClick}>Click</button>
-			</div> */}
-			{/* <Head /> */}
-			<Nav />
-            <Body />
-        </div>
-    );
+	return (
+		<ThemeContext.Provider value={{ theme, toggleTheme }}>
+
+			<div className="App" id={theme}>
+
+				<div id="navdiv">
+					<div className="row">
+
+
+
+						<div className="col col-sm">
+							<h1 id="greettime" style={customStyle}>
+								{greeting} <span id="time"></span>
+							</h1>
+						</div>
+
+
+						<div className="col col-sm-2 nav">
+							<ul>
+								
+								<li className="dash">_</li>
+								<li><a href="#About Me">About Me</a></li>
+								<li><a href="#Work">Work</a></li>
+								<li><a href="#Contact-form">Contact</a></li>
+								<li><a href="#Resume">Resume</a></li>
+								<li><a href="#Top">Top</a></li>
+								<br></br>
+								<br></br>
+								
+								<li><a class="socials" href="https://www.linkedin.com/in/donghoh-han-7b3a4656/">LINKEDIN</a></li>
+								<li><a class="socials" href="https://github.com/donghohnation">GITHUB</a></li>
+								<li><a class="socials" href="https://www.behance.net/donghohnation/">BEHANCE</a></li>
+								<br></br>
+								<br></br>
+								
+								<div className="switch">
+									<label id="lightdark"> {theme === "light" ? "TOGGLE DARK MODE" : "TOGGLE LIGHT MODE"}</label>
+									<div>
+									<ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+									</div>
+								</div>
+
+							</ul>
+						</div>
+
+
+					</div>
+					<div>
+						<img id="avatar" src={ava} alt='avatar'></img>
+					</div>
+				</div>
+
+
+
+
+
+
+
+
+
+
+
+				{/* <Nav /> */}
+				<Body />
+			</div>
+
+		</ThemeContext.Provider>
+	);
 
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,38 +10,11 @@ function Nav() {
 	
 	const date = new Date();
 	const currentTime = date.getHours();
-	// let now = date.toLocaleTimeString();
 	
 	
 	function theTime() {
 		let date = new Date();
 		let rightnow = date.toLocaleTimeString();
-		// let mm = ""
-		// let H = Datte.getHours();
-		// let m = Datte.getMinutes(); 
-		// let s = Datte.getSeconds();
-		// if (H < 12) {
-		// 	mm = "AM";
-		// }
-		// if (H === 0) {
-		// 	H = 12;
-		// }
-		// if (H > 12) {
-		// 	mm = "PM";
-		// }
-		// if (H > 12) {
-		// 	H = H - 12;
-		// }
-		// if (H < 10) {
-		// 	H = "0" + H;
-		// }
-		// if (m < 10) {
-		// 	m = "0" + m;
-		// }
-		// if (s < 10) {
-		// 	s = "0" + s;
-		// }
-		// document.getElementById("time").textContent = `${H}:${m}:${s} ${mm}`
 		document.getElementById("time").textContent = `${rightnow}`
 	}
 	setInterval(theTime);
